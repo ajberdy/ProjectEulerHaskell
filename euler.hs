1e861e4015561bada255a4c0498db0a3013b7178
Aaron Berdy <aaronberdy@ucla.edu>
2019-03-27 04:13:47 -0700

    problem 5

--- euler.hs
+++ euler.hs
@@ -70,6 +70,8 @@ getAlgorithm :: (Integral a, Num b) => a -> Maybe b
 getAlgorithm 1 = Just $ fromIntegral problem_1
 getAlgorithm 2 = Just $ fromIntegral problem_2
 getAlgorithm 3 = Just $ fromIntegral problem_3
+getAlgorithm 4 = Just $ fromIntegral problem_4
+getAlgorithm 5 = Just $ fromIntegral problem_5
 getAlgorithm x = Nothing
 
 main = do
@@ -126,18 +128,20 @@ ackerman 0 y = y + 1
 ackerman x 0 = ackerman (x - 1) 1
 ackerman x y = ackerman (x - 1) $ ackerman x (y - 1)
 
-smallestFactor :: Integer -> Integer
-smallestFactor n = head $ filter modCheck [2..]
-  where modCheck x = n `mod` x == 0
+goesInto :: Integer -> Integer -> Bool
+goesInto n p = n `mod` p == 0
 
-primeFactorization :: Integer -> [Integer]
-primeFactorization n
+smallestFactor :: Integer -> Integer
+smallestFactor n = head $ filter (goesInto n) [2..]
+
+primeFactorizationList :: Integer -> [Integer]
+primeFactorizationList n
   | p == n    = [n]
-  | otherwise = p : (primeFactorization $ n `div` p)
+  | otherwise = p : (primeFactorizationList $ n `div` p)
   where p = smallestFactor n
 
 largestFactor :: Integer -> Integer
-largestFactor n = last $ primeFactorization n
+largestFactor n = last $ primeFactorizationList n
 
 problem_3 :: Integer
 problem_3 = largestFactor 600851475143
@@ -149,4 +153,39 @@ isPalindrome x = xStr == reverse xStr
 problem_4 :: Integer
 problem_4 = maximum palindromes
   where palindromes = filter isPalindrome [a * b | a <- [100..999], b <- [100..a]]
+
+goesIntoPower :: Integer -> Integer -> Integer
+goesIntoPower n x =
+  if goesInto n x then
+    1 + goesIntoPower (n `div` x) x
+  else 0
+
+primeFactorization :: Integer -> [(Integer, Integer)]
+primeFactorization n
+  | n == 1    = []
+  | p == n    = [(n, 1)]
+  | otherwise = (p, highestPower) : (primeFactorization $ n `div` (p^highestPower))
+  where p = smallestFactor n
+        highestPower = goesIntoPower n p
+
+leastCommonMultiple :: Integer -> Integer -> Integer
+leastCommonMultiple a b =
+  foldl factorIn 1 lCMFactors
+  where
+    factorIn x (p, power) = x * p^power
+    lCMFactors = findLCMFactors (primeFactorization a) (primeFactorization b)
+
+findLCMFactors :: [(Integer, Integer)] -> [(Integer, Integer)] -> [(Integer, Integer)]
+findLCMFactors aFactors [] = aFactors
+findLCMFactors [] bFactors = bFactors
+findLCMFactors ((aFactor, aPower):aRest) ((bFactor, bPower):bRest)
+  | aFactor < bFactor =
+    (aFactor, aPower) : (findLCMFactors aRest ((bFactor, bPower):bRest))
+  | aFactor == bFactor =
+    (aFactor, max aPower bPower) : (findLCMFactors aRest bRest)
+  | otherwise =
+    (bFactor, bPower) : (findLCMFactors ((aFactor, aPower):aRest) bRest)
+
+problem_5 :: Integer
+problem_5 = foldl1 leastCommonMultiple [2..20]
 
